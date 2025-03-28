67cbbc697a20b772f631af57efaa92ec56db9715
renlo3000 <152633649+renlo3000@users.noreply.github.com>
2025-03-28 16:09:20 +0000

    cambios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+// import CSS
+import 'bulma/css/bulma.css'
+
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
 
-//impirtar las imagenes y asignarles un objeto
+// Importamos las imagenes
 import MarioImage from './imagenes/Mario.png'
 import LuigiImage from './imagenes/Luigi.png'
 import PeachesImage from './imagenes/Peaches.png'
+
 export function App() {
   return (
     <div>
         <div>Personajes de Mario Bros</div>
-        <Tarjeta nombre= "Mario" colorFavorito="Rojo" image={MarioImage}/>
-        <Tarjeta nombre= "Luigi" colorFavorito="Green"image={LuigiImage}/>
-        <Tarjeta nombre= "Peach" colorFavorito="Rosa"image={PeachesImage}/>
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-2">
+                <Tarjeta nombre="Mario" colorFavorito="Rojo" image={MarioImage}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Luigi" colorFavorito="Verde" image={LuigiImage}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Peaches" colorFavorito="Rosa" image={PeachesImage}/>
+              </div>
+            </div>
+          </section>
+        </div>
     </div>
+  
   )
 }
