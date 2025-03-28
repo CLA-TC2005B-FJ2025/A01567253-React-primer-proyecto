11aa71420b3201d904df4483e5d2fda92a15462b
renlo3000 <152633649+renlo3000@users.noreply.github.com>
2025-03-28 16:03:20 +0000

    bulma

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
 
+//impirtar las imagenes y asignarles un objeto
+import MarioImage from './imagenes/Mario.png'
+import LuigiImage from './imagenes/Luigi.png'
+import PeachesImage from './imagenes/Peaches.png'
 export function App() {
   return (
     <div>
         <div>Personajes de Mario Bros</div>
-        <Tarjeta nombre= "Mario" colorFavorito="Rojo"/>
-        <Tarjeta nombre= "Luigi" colorFavorito="Green" />
-        <Tarjeta nombre= "Peach" colorFavorito="Rosa"/>
+        <Tarjeta nombre= "Mario" colorFavorito="Rojo" image={MarioImage}/>
+        <Tarjeta nombre= "Luigi" colorFavorito="Green"image={LuigiImage}/>
+        <Tarjeta nombre= "Peach" colorFavorito="Rosa"image={PeachesImage}/>
     </div>
   )
 }
